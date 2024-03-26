99c7d63605b9fee3b76eb798aca5ae50b37871a9
bani6809 <43738861+bani6809@users.noreply.github.com>
2024-03-26 11:01:19 -0700

    add S99perf_log (#119)
    * add S99perf_log
    
    * use flag file

---
+++ images/cfw/opt/perf_log.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+DEVICE_SN=$(cat /proc/cmdline | xargs -n1 | grep "bbl_serial=" | sed "s|bbl_serial=||")
+
+LOGFILE=/mnt/sdcard/x1plus/printers/$DEVICE_SN/logs/perf_log.txt
+
+date > $LOGFILE
+while true ; do
+	free >> $LOGFILE
+	ps -auwxf >> $LOGFILE
+	sleep 30
+done
