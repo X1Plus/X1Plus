b13024400953f8f50006fee6f0f6ed0589adcef5
Joshua Wise <joshua@joshuawise.com>
2024-03-28 19:03:21 -0400

    interpose both the weird device manager and the real one, successfully inject language type lolol

--- bbl_screen-patch/interpose.cpp
+++ bbl_screen-patch/interpose.cpp
@@ -569,39 +569,47 @@ SWIZZLE(void *, _ZN7QObjectC2EPS_, void *p1, void *p2)
     return next(p1, p2);
 }
 
-static void **DeviceManager;
 static QMap<QByteArray, QString> *langmap;
 
-extern "C" void * _ZN12QMapDataBase11shared_nullE;
-
-#include <QtCore/QTranslator>
-
-SWIZZLE(bool, _ZN11QTranslator4loadERK7QStringS2_S2_S2_, QTranslator *t, QString const& a, QString const& b, QString const& c, QString const& d)
-    printf("qTranslator::load called on\n");
-    qDebug() << a << b << c << d;
-    return next(t, a, b, c, d);
+SWIZZLE(void *, _Z12bbl_get_propNSt7__cxx1112basic_stringIcSt11char_traitsIcESaIcEEES4_bb, void *a, std::__cxx11::basic_string<char, std::char_traits<char>, std::allocator<char> > *s1, std::__cxx11::basic_string<char, std::char_traits<char>, std::allocator<char> > *s2, bool b1, bool b2)
+    if (lang_init == 3 && *s1 == "device/model_int") {
+        printf("LANG INTERPOSE: found init for DeviceManager\n");
+        void **DeviceManager = (void **)last_qobject;
+        langmap = (QMap<QByteArray, QString> *) (DeviceManager + 9);
+        lang_init++;
+    }
+    return next(a, s1, s2, b1, b2);
 }
 
 SWIZZLE(void *, _ZN10QByteArrayC1EPKci, void *qba, const char *s, int n)
-    if (lang_init == 0 && s && strcmp(s, "en") == 0) {
-        printf("saw the first pass of en, the last QObject must have been deviceManager\n");
-        DeviceManager = (void **)last_qobject;
-        lang_init++;
+    if (s && strcmp(s, "en") == 0) {
+        if (lang_init == 0) {
+            printf("LANG INTERPOSE: saw the first pass of en, the last QObject must have been the weird not-a-deviceManager\n");
+            void **DeviceManager = (void **)last_qobject;
+            langmap = (QMap<QByteArray, QString> *) (DeviceManager + 5);
+            lang_init++;
+        } else if (lang_init == 1) {
+            printf("LANG INTERPOSE: here is the actual langmap init en for round 1\n");
+            lang_init++;
+        } else if (lang_init == 3) {
+            printf("LANG INTERPOSE: saw en for round 1 language default initializer\n");
+            lang_init++;
+        } else if (lang_init == 4) {
+            printf("LANG INTERPOSE: saw en for round 2 map initializer\n");
+            lang_init++;
+        } else {
+            printf("LANG INTERPOSE: saw en fly by again... but after lang init?\n");
+        }
     }
-    if (lang_init == 1 && s && strcmp(s, "sv") == 0) {
-        langmap = (QMap<QByteArray, QString> *) (DeviceManager + 5);
-        printf("DeviceManager's maps are probably ready, let's do it. langmap = %p, *(uint32_t*)*langmap = %p, _ZN12QMapDataBase11shared_nullE = %p\n",
-            langmap,
-            *(void **)langmap,
-            &_ZN12QMapDataBase11shared_nullE);
-        (*langmap)["lo"] = "lolol";
-        qDebug() << *langmap;
-        lang_init++;
-    }
-    if (lang_init == 2 && s && strcmp(s, "ja") == 0) {
-        printf("adding the last one, here we go\n");
-        qDebug() << *langmap;
-        lang_init++;
+    if (s && strcmp(s, "sv") == 0) {
+        if (lang_init == 2 || lang_init == 5) {
+            printf("LANG INTERPOSE: DeviceManager's maps are probably ready, let's do it. langmap = %p, contents are now ", langmap);
+            (*langmap)["lo"] = "lolol";
+            qDebug() << *langmap;
+            lang_init++;
+        } else {
+            printf("LANG INTERPOSE: saw sv fly by again... but after lang init?\n");
+        }
     }
 
     return next(qba, s, n);
