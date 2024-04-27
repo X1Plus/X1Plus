2f256d044e0dffe083bc94f7eb2196cadc1ffbf7
Joshua Wise <joshua@joshuawise.com>
2024-04-27 01:17:05 -0700

    use tar2sqfs instead of gensquashfs (#219)
    gensquashfs had a bunch of misbehavior in the past, including generating duplicates of /opt/python/bin. Instead, we do something truly terrible, involving generating a tar file with what we want, and then packing that. This sucks but the only thing that sucks worse is what we had, I guess.
    
    At some point the tarfile generation stuff can be refactored out, once we have multiple images. But this is fine for now and unblocks what I was actually trying to do with my night.
    
    * crontab: make +x
    * iwconfig: make +x
    * squashfs repack: switch to tar2sqfs instead of gensquashfs, which generated weird broken images
    * cron: use correct DEVICE_SN

---
+++ images/cfw.py
@@ -0,0 +1,128 @@
+#!/usr/bin/env python3
+import tarfile
+from tarfile import TarFile, TarInfo
+import sys
+import io
+import glob
+import os
+
+# This sucks.  I wish the packfile support for gensquashfs were less buggy. 
+# The glob support is really pretty bad.  But here we are.  We hand-generate
+# a tar file, with the files exactly where we want them, that we then feed
+# to tar2sqfs.
+#
+# One silver lining is that it means that we don't have to leave an unpacked
+# copy of the Python runtime lying around that can get out of sync and/or
+# out of date -- we always pack directly from the prebuilt.  Hooray us, I
+# guess!
+
+mtime = os.stat(__file__).st_mtime
+
+bio = io.BytesIO()
+
+tf = TarFile(fileobj = bio, mode = 'w', format = tarfile.GNU_FORMAT)
+
+def dir(dirname):
+    ti = TarInfo(dirname)
+    ti.type = tarfile.DIRTYPE
+    ti.mode = 0o755
+    ti.uid = 0
+    ti.gid = 0
+    ti.mtime = mtime
+    tf.addfile(ti)
+
+def whiteout(path):
+    ti = TarInfo(path)
+    ti.type = tarfile.CHRTYPE
+    ti.devmajor = 0
+    ti.devminor = 0
+    ti.mode = 0o0
+    ti.uid = 0
+    ti.gid = 0
+    ti.mtime = mtime
+    tf.addfile(ti)
+
+def symlink(src, dest):
+    ti = TarInfo(src)
+    ti.type = tarfile.SYMTYPE
+    ti.linkname = dest
+    ti.mode = 0o777
+    ti.uid = 0
+    ti.gid = 0
+    ti.mtime = mtime
+    tf.addfile(ti)
+
+def globfiles(fromdir, todir, eatlinks = []):
+    for f in glob.glob('**', root_dir = fromdir, recursive = True):
+        if f[-1] == "~":
+            continue
+        inf = f"{fromdir}/{f}"
+        ti = tf.gettarinfo(name = inf, arcname = f"{todir}/{f}")
+        if (eatlinks == True or (f in eatlinks)) and ti.type == tarfile.SYMTYPE:
+            ti = tf.gettarinfo(name = os.path.join(os.path.dirname(inf), os.readlink(inf)), arcname = f"{todir}/{f}")
+        ti.uid = 0
+        ti.gid = 0
+        ti.uname = "root"
+        ti.gname = "root"
+        ti.mtime = mtime
+        if ti.isfile():
+            with open(inf, 'rb') as infd:
+                tf.addfile(ti, fileobj = infd)
+        else:
+            tf.addfile(ti)
+
+symlink('/root/.ssh', '/config/sshd')
+symlink('/etc/localtime', '/usr/share/zoneinfo/Etc/UTC')
+symlink('/var/run/utmp', '/dev/null')
+symlink('/usr/sbin/mount.smb3', '/usr/sbin/mount.cifs')
+
+whiteout('/etc/init.d/S50fcgiwrap')
+whiteout('/usr/bin/wl')
+whiteout('/lib/optee_armtz/380231ac-fb99-47ad-a689-9e017eb6e78a.ta')
+whiteout('/lib/optee_armtz/4367fd45-4469-42a6-925d-3857b952704a.ta')
+whiteout('/lib/optee_armtz/528938ce-fc59-11e8-8eb2-f2801f1b9fd1.ta')
+whiteout('/lib/optee_armtz/5b9e0e40-2636-11e1-ad9e-0002a5d5c51b.ta')
+whiteout('/lib/optee_armtz/5ce0c432-0ab0-40e5-a056-782ca0e6aba2.ta')
+whiteout('/lib/optee_armtz/614789f2-39c0-4ebf-b235-92b32ac107ed.ta')
+whiteout('/lib/optee_armtz/731e279e-aafb-4575-a771-38caa6f0cca6.ta')
+whiteout('/lib/optee_armtz/873bcd08-c2c3-11e6-a937-d0bf9c45c61c.ta')
+whiteout('/lib/optee_armtz/8cccf200-2450-11e4-abe2-0002a5d5c52c.ta')
+whiteout('/lib/optee_armtz/8dddf200-2450-11e4-abe2-0002a5d5c53d.ta')
+whiteout('/lib/optee_armtz/a4c04d50-f180-11e8-8eb2-f2801f1b9fd1.ta')
+whiteout('/lib/optee_armtz/b3091a65-9751-4784-abf7-0298a7cc35ba.ta')
+whiteout('/lib/optee_armtz/c3f6e2c0-3548-11e1-b86c-0800200c9a66.ta')
+whiteout('/lib/optee_armtz/d17f73a0-36ef-11e1-984a-0002a5d5c51b.ta')
+whiteout('/lib/optee_armtz/dc6b2622-2260-4da6-9f7a-d8d5fa4d2a1e.ta')
+whiteout('/lib/optee_armtz/dc6b2623-2260-4da6-9f7a-d8d5fa4d2a1e.ta')
+whiteout('/lib/optee_armtz/e13010e0-2ae1-11e5-896a-0002a5d5c51b.ta')
+whiteout('/lib/optee_armtz/e626662e-c0e2-485c-b8c8-09fbce6edf3d.ta')
+whiteout('/lib/optee_armtz/e6a33ed4-562b-463a-bb7e-ff5e15a493c8.ta')
+whiteout('/lib/optee_armtz/f157cda0-550c-11e5-a6fa-0002a5d5c51b.ta')
+whiteout('/lib/optee_armtz/ffd2bded-ab7d-4988-95ee-e4962fff7154.ta')
+
+globfiles("cfw/etc", "/etc")
+globfiles("cfw/usr", "/usr", eatlinks = [ 'bin/jq' ])
+globfiles("cfw/lib", "/lib")
+globfiles("cfw/sbin", "/sbin")
+globfiles("cfw/system", "/system", eatlinks = True)
+globfiles("cfw/opt", "/opt", eatlinks = True)
+
+with tarfile.open('../prebuilt/python3.tar.gz', 'r') as itf:
+    for ti in itf.getmembers():
+        if ti.name[:2] == './':
+            ti.name = ti.name[2:]
+        if ti.name == '.':
+            continue
+        ti.name = "/opt/python/" + ti.name
+        ti.uname = "root"
+        ti.gname = "root"
+        ti.uid = 0
+        ti.gid = 0
+        ti.mtime = mtime
+        if ti.isfile():
+            tf.addfile(ti, itf.extractfile(ti))
+        else:
+            tf.addfile(ti)
+
+tf.close()
+sys.stdout.buffer.write(bio.getbuffer())
