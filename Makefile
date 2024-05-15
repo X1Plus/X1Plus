4dc1c96ca35aef82d3cfbb653539fc937828cfec
Joshua Wise <joshua@joshuawise.com>
2024-05-15 14:51:39 -0400

    mkotajson: take an arbitrary x1p and make an ota.json

--- Makefile
+++ Makefile
@@ -28,7 +28,7 @@ define make =
 	$(MAKE) -C $(1) $(MAKEFLAGS) FIRMWARE=$(FIRMWARE) $(2)
 endef
 
-all: $(CFWVERSION).x1p
+all: $(CFWVERSION).x1p ota.json
 	make -C installer-clientside/stage1
 
 # needs:
@@ -41,7 +41,9 @@ all: $(CFWVERSION).x1p
 #   images/cfw.squashfs
 $(CFWVERSION).x1p: images initramfs bbl_screen-patch
 	scripts/mkx1p.py $@
-	scripts/mkotajson.py
+
+ota.json: $(CFWVERSION).x1p
+	scripts/mkotajson $< $@
 
 bbl_screen-patch: firmwares
 
--- scripts/mkotajson.py
+++ scripts/mkotajson.py
@@ -1,37 +1,30 @@
 #!/usr/bin/env python3
 
+import sys
 import os
 import json
 import hashlib
+import zipfile
 
-ROOTPATH = os.path.abspath(os.path.join(os.path.dirname(os.path.abspath(__file__)), ".."))
-
-# Load info.json
-try:
-    INFO = json.load(open(f"{ROOTPATH}/installer/info.json", "r"))
-except Exception as e:
-    print(f"Error loading info.json, has mkinfojson.sh ran yet?! Error of: {e}")
-    raise
+with zipfile.ZipFile(sys.argv[1], 'r') as zf:
+    with zf.open('info.json') as f:
+        info = json.load(f)
 
 # MD5sum our OTA file
-try:
-    OTA_MD5 = hashlib.md5(open(f"{ROOTPATH}/{INFO['cfwVersion']}.x1p",'rb').read()).hexdigest()
-except Exception as e:
-    print(f"Error loading {INFO.cfwVersion}.x1p, did the x1p build correctly?! Error of: {e}")
-    raise
+ota_md5 = hashlib.md5(open(sys.argv[1], 'rb').read()).hexdigest()
 
 # Generate OTA file
 json.dump(
     {
-        "cfwVersion": INFO['cfwVersion'],
-        "date": INFO['date'],
-        "buildTimestamp": INFO['buildTimestamp'],
-        "notes": INFO['notes'],
-        "ota_url": f"https://github.com/X1Plus/X1Plus/releases/download/x1plus%2F{INFO['cfwVersion']}/{INFO['cfwVersion']}.x1p",
-        "ota_md5": OTA_MD5,
-        "base_update_url": INFO['base']['updateUrl'],
-        "base_update_md5": INFO['base']['updateMd5'],
+        "cfwVersion": info['cfwVersion'],
+        "date": info['date'],
+        "buildTimestamp": info['buildTimestamp'],
+        "notes": info['notes'],
+        "ota_url": f"https://github.com/X1Plus/X1Plus/releases/download/x1plus%2F{info['cfwVersion']}/{info['cfwVersion']}.x1p",
+        "ota_md5": ota_md5,
+        "base_update_url": info['base']['updateUrl'],
+        "base_update_md5": info['base']['updateMd5'],
     },
-    open(f"{ROOTPATH}/ota.json", "w"),
+    open(sys.argv[2], "w"),
     indent = 4,
 )
