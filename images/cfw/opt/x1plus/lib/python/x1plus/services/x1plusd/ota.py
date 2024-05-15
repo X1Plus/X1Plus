fb35e23184de671a2d940ed317edd2d1bdab6e6e
Joshua Wise <joshua@joshuawise.com>
2024-05-15 03:42:07 -0400

    x1plusd: introduce settings.on mechanism, and have ota use settings.on to react to changes in ota.enabled, and also change the verb tense of "enabled" to be consistent with homescreen.graph

--- images/cfw/opt/x1plus/lib/python/x1plus/services/x1plusd/ota.py
+++ images/cfw/opt/x1plus/lib/python/x1plus/services/x1plusd/ota.py
@@ -50,6 +50,8 @@ class OTAService(X1PlusDBusService):
             dbus_interface=OTA_INTERFACE, dbus_path=OTA_PATH, **kwargs
         )
 
+    def ota_enabled(self):
+        return self.x1psettings.get("ota.enabled", False)
 
     async def task(self):
         # On startup run an update check to populate info
@@ -59,7 +61,7 @@ class OTAService(X1PlusDBusService):
     async def dbus_CheckNow(self, req):
         self.next_check_timestamp = datetime.datetime.now()
         self.ota_task_wake.set()
-        return {"CheckNow": "Triggered"}
+        return {"status": "ok" if self.ota_enabled() else "disabled"}
 
     async def dbus_GetStatus(self, req):
         return {
@@ -99,16 +101,18 @@ class OTAService(X1PlusDBusService):
         )
     
     async def _ota_task(self):
-        # Do we have OTAs enabled? If not, just return current status
-        if not self.x1psettings.get("ota.enable", False):
-            # TODO: subscribe to ota.enable changes, somehow
-            logger.info("OTA check is disabled, skipping check!")
-            return
+        # Make sure that we are given a chance to see if there is work to do
+        # when the OTA status changes.
+        self.x1psettings.on("ota.enabled", lambda: self.ota_task_wake.set())
         
         while True:
             did_work = False
+            ota_enabled = self.ota_enabled()
+            
+            if not ota_enabled:
+                logger.debug(f"OTA engine is disabled, so we are definitely doing nothing")
 
-            if datetime.datetime.now() > self.next_check_timestamp:
+            if ota_enabled and datetime.datetime.now() > self.next_check_timestamp:
                 await self._check_for_ota()
                 did_work = True
             
@@ -120,7 +124,11 @@ class OTAService(X1PlusDBusService):
                 # cooperatively multitasked (i.e., we will continue until we
                 # yield)
                 now = datetime.datetime.now()
-                next_work = max((self.next_check_timestamp,))
+                
+                next_work = now + datetime.timedelta(days = 365) # really, "forever"
+                if ota_enabled:
+                    next_work = min((next_work, self.next_check_timestamp,))
+
                 logger.debug(f"going to sleep for {next_work - now}")
                 try:
                     await asyncio.wait_for(self.ota_task_wake.wait(), timeout = (next_work - now).total_seconds())
