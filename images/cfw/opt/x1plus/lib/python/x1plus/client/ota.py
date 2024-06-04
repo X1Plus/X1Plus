9cca38b8068e60528826c3ae60db447ff593bd2c
Joshua Wise <joshua@joshuawise.com>
2024-06-04 05:58:42 -0400

    x1plus.client.ota: implement other commands

--- images/cfw/opt/x1plus/lib/python/x1plus/client/ota.py
+++ images/cfw/opt/x1plus/lib/python/x1plus/client/ota.py
@@ -1,4 +1,5 @@
 from .base import call
+from . import settings
 
 from jeepney import DBusAddress
 
@@ -74,6 +75,66 @@ def _cmd_check(args):
     print("")
     _cmd_status()
 
+def _cmd_download(args):
+    status = get_status()
+
+    if status['status'] != 'IDLE':
+        print("OTA engine is not idle -- cannot start download.")
+        return
+    
+    if not status['ota_available']:
+        print("No updates are available.")
+        return
+    
+    if status['ota_is_downloaded'] and (status['ota_base_is_downloaded'] or not args.base):
+        print("Nothing to do: all requested firmware is already downloaded.")
+        return
+    
+    print("Starting OTA download.")
+    download(base_firmware = args.base)
+    
+    print("Waiting for download to complete...")
+    while True:
+        status = get_status()
+        if status['status'] == 'IDLE':
+            break
+        print(f"Status: {status['status']}: {status['download']['bytes']}/{status['download']['bytes_total']} bytes...")
+        time.sleep(0.5)
+
+    # XXX: check status['download']['last_error']?    
+    _cmd_status()
+
+def _cmd_update(args):
+    status = get_status()
+
+    if status['status'] != 'IDLE':
+        print("OTA engine is not idle -- cannot start update.")
+        return
+    
+    if not status['ota_available']:
+        print("No updates are available.")
+        return
+    
+    if not (status['ota_is_downloaded'] and status['ota_base_is_downloaded']):
+        print("Update is not fully downloaded -- cannot install.")
+        return
+
+    print(f"Rebooting to install X1Plus version {status['ota_info']['cfwVersion']} in 5 seconds -- press ctrl-C to abort!")
+    time.sleep(5)
+    
+    update()
+
+def _cmd_set_url(args):
+    if args.url == "":
+        args.url = None
+    
+    if args.url is None:
+        print("Resetting ota.json URL to default.")
+    else:
+        print(f"Setting ota.json URL to \"{args.url}\".")
+    
+    settings.put("ota.json_url", args.url)
+
 def add_subparser(subparsers):
     ota_parser = subparsers.add_parser('ota', help="manage Over-The-Air update engine")
     ota_subparsers = ota_parser.add_subparsers(title = 'subcommands', required = True)
@@ -86,5 +147,11 @@ def add_subparser(subparsers):
 
     ota_download_parser = ota_subparsers.add_parser('download', help="start an update downloading, if one is available")
     ota_download_parser.add_argument('--base', action="store_true", help="download base firmware as well as X1Plus update, if needed")
+    ota_download_parser.set_defaults(func=_cmd_download)
 
     ota_update_parser = ota_subparsers.add_parser('update', help="immediately apply a pending update, if one is available")
+    ota_update_parser.set_defaults(func=_cmd_update)
+    
+    ota_set_url_parser = ota_subparsers.add_parser('set-url', help="change the URL to check for updates from")
+    ota_set_url_parser.add_argument('url', action="store", nargs="?", help="URL for an ota.json; if not specified, returns to X1Plus defaults")
+    ota_set_url_parser.set_defaults(func=_cmd_set_url)
