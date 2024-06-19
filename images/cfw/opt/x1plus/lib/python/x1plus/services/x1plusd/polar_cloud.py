bb615f2c32ede52c1b5d0a830f6101e8daf8ace0
Eric Ford <ericford@mac.com>
2024-06-19 14:34:02 -0400

    Added _on_welcome() and necessary dependencies.

--- images/cfw/opt/x1plus/lib/python/x1plus/services/x1plusd/polar_cloud.py
+++ images/cfw/opt/x1plus/lib/python/x1plus/services/x1plusd/polar_cloud.py
@@ -37,7 +37,67 @@ class PolarPrintService:
     async def begin(self):
         """Create Socket.IO client and connect to server."""
         self.socket = socketio.AsyncClient()
+        self.set_interface()
         connect_task = asyncio.create_task(
             self.socket.connect(self.server_url, transports=["websocket"])
         )
         await connect_task
+        self.socket.on("welcome", self._on_welcome)
+
+
+    async def _on_welcome(self, response, *args, **kwargs):
+        """
+        Check to see if printer has already been registered. If it has, we can
+        ignore this. Otherwise, must get a key pair, then call register.
+        """
+        logger.info("_on_welcome.")
+        logger.debug(f"challenge: {response['challenge']}")
+        logger.debug(
+            f"Polar SN: {self.polar_sn}, Public key exists: {bool(self.public_key)}"
+        )
+        # Two possibilities here. If it's already registered there should be a
+        # Polar Cloud serial number and a set of RSA keys. If not, then must
+        # request keys first.
+        if self.polar_sn and self.public_key:
+            # The printer has been registered. Note that
+            # we're now using the serial number assigned by the server.
+            # First, encode challenge string with the private key.
+            logger.debug(f"_on_welcome Polar Cloud SN: {self.polar_sn}")
+            cipher_rsa = PKCS1_OAEP.new(self.private_key)
+            encrypted = b64encode(cipher_rsa.encrypt(response["challenge"]))
+            logger.debug(f"_on_welcome encrypted challenge: {encrypted}")
+            data = {
+                "serialNumber": self.polar_sn,
+                "signature": encrypted,  # BASE64 encoded string
+                "MAC": self.mac,
+                "protocol": "2.0",
+                "mfgSn": self.polar_sn,
+            }
+            """
+            Note that the following optional fields might be used in future.
+            "printerMake": "printer make",                     // string, optional
+            "version": "currently installed software version", // string, optional
+            "localIP": "printer's local IP address",           // string, optional
+            "rotateImg": 0 | 1,                                // integer, optional
+            "transformImg": 0 - 7,                             // integer, optional
+            "camOff": 0 | 1,                                   // integer, optional
+            "camUrl": "URL for printer's live camera feed"     // string, optional
+            """
+
+            # Send hello request.
+            await self.socket.emit("hello", data)
+        elif not self.polar_sn and not self.public_key:
+            # We need to get an RSA key pair before we can go further.
+            await self.socket.emit("makeKeyPair", {"type": "RSA", "bits": 2048})
+        # elif not self.polar_sn:
+        #     # We already have a key: just register. Technically, there should be
+        #     # no way to get here. Included for completion.
+        #     logger.error(
+        #         "_on_welcome Somehow there are keys with no SN. Reregistering."
+        #     )
+        #     await self._register()
+
+    def set_interface(self):
+        """Get IP and MAC addresses and store them in self.settings."""
+        self.mac = get_MAC()
+        self.ip = get_IP()
