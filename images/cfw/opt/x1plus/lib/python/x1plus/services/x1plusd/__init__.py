011e36744e68101ae28891974fb8efdcd7d56b07
Eric Ford <ima-hima@users.noreply.github.com>
2024-06-20 09:26:51 -0400

    Polar/add connect (#314)
    This code allows printers running X1Plus to connect to Polar Cloud. Currently, it relies on a hard-coded .env file for creds. This will need to be addressed in a future update. Also for a future update, actually being able to print!
    
    ---------
    
    Co-authored-by: Joshua Wise <joshua@joshuawise.com>

--- images/cfw/opt/x1plus/lib/python/x1plus/services/x1plusd/__init__.py
+++ images/cfw/opt/x1plus/lib/python/x1plus/services/x1plusd/__init__.py
@@ -21,5 +21,9 @@ async def main():
 
     asyncio.create_task(settings.task())
     asyncio.create_task(ota.task())
+    if settings.get("polar_cloud", False):
+        from .polar_cloud import PolarPrintService
+        polar_cloud = PolarPrintService(settings=settings)
+        asyncio.create_task(polar_cloud.begin())
 
     logger.info("x1plusd is running")
