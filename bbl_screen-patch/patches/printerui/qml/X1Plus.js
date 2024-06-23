08c037a957a4aa9da697e2b3760be0b816af0154
Joshua Wise <joshua@joshuawise.com>
2024-06-23 00:13:51 -0700

    Add support for USB Ethernet adapters on X1 Carbon (#320)
    In theory, only the X1E has support for Ethernet.  As it turns out, vestiges of this support live in the X1 Carbon firmware, but it needs some help.  To help it out, we:
    
    * Interpose `libbbl_utils`, which, on X1 Carbon, doesn't know that `eth0` being up means that the network could be up.  `device_gate` needs to know this in order to be willing to connect to the cloud over Ethernet (thanks @balika011).
    * Binary-patch `netService`, which doesn't know to create an `eth0` route on X1 Carbon, but can be convinced to with a little bit of help (thanks @balika011).
    * Look for the X1Plus Expansion Board on boot, and if one is connected that doesn't have a MAC address in its EEPROM, assign an LAA based on the printer serial number so we get a consistent IP address each time we boot.
    * Write some QML to listen to `netService`, because `bbl_screen` does not know how to listen for a `NetworkManager.wiredNetwork`; we create an `X1Plus.Network.wiredNetwork()` interface that looks a lot like a `NetworkManager.wiredNetwork` in response to `netService` DDS events, and replace JavaScript lookups of `NetworkManager.wiredNetwork` to point to this interface.
    
    This allows X1 Carbons augmented with a USB Ethernet adapter of any kind (including an X1Plus Expansion Board) to use the native X1E Ethernet infrastructure to connect instead of WiFi, and provides the X1E GUI when an `eth0` interface of some kind is present.
    
    Co-authored-by: Balázs Triszka <balika011@gmail.com>

--- bbl_screen-patch/patches/printerui/qml/X1Plus.js
+++ bbl_screen-patch/patches/printerui/qml/X1Plus.js
@@ -13,6 +13,7 @@
 .import "./x1plus/Settings.js" as X1PlusSettings
 .import "./x1plus/TempGraph.js" as X1PlusTempGraph
 .import "./x1plus/OTA.js" as X1PlusOTA
+.import "./x1plus/Network.js" as X1PlusNetwork
 
 /* Back-end model logic for X1Plus's UI
  *
@@ -60,6 +61,8 @@ X1Plus.TempGraph = X1PlusTempGraph;
 var TempGraph = X1PlusTempGraph;
 X1Plus.OTA = X1PlusOTA;
 var OTA = X1PlusOTA;
+X1Plus.Network = X1PlusNetwork;
+var Network = X1PlusNetwork;
 
 Stats.X1Plus = X1Plus;
 DDS.X1Plus = X1Plus;
@@ -70,12 +73,15 @@ DBus.X1Plus = X1Plus;
 Settings.X1Plus = X1Plus;
 TempGraph.X1Plus = X1Plus;
 OTA.X1Plus = X1Plus;
+Network.X1Plus = X1Plus;
 
 var _DdsListener = JSDdsListener.DdsListener;
 var _X1PlusNative = JSX1PlusNative.X1PlusNative;
 var DeviceManager = null;
 var PrintManager = null;
+var NetworkManager = null;
 var PrintTask = null;
+var NetworkEnum = null;
 var printerConfigDir = null;
 
 var emulating = _X1PlusNative.getenv("EMULATION_WORKAROUNDS");
@@ -148,11 +154,13 @@ X1Plus.fileExists = fileExists;
  * they are loaded, and they might need to do work touching other modules. 
  * These things happen from 'awaken'.
  */
-function awaken(_DeviceManager, _PrintManager, _PrintTask) {
+function awaken(_DeviceManager, _PrintManager, _NetworkManager, _PrintTask, _Network) {
 	console.log("X1Plus.js awakening");
 	X1Plus.DeviceManager = DeviceManager = _DeviceManager;
 	X1Plus.PrintManager = PrintManager = _PrintManager;
+	X1Plus.NetworkManager = NetworkManager = _NetworkManager;
 	X1Plus.PrintTask = PrintTask = _PrintTask;
+	X1Plus.NetworkEnum = NetworkEnum = _Network;
 	X1Plus.printerConfigDir = printerConfigDir = `/mnt/sdcard/x1plus/printers/${X1Plus.DeviceManager.build.seriaNO}`;
 	_X1PlusNative.system("mkdir -p " + _X1PlusNative.getenv("EMULATION_WORKAROUNDS") + printerConfigDir);
 	Settings.awaken();
@@ -161,6 +169,7 @@ function awaken(_DeviceManager, _PrintManager, _PrintTask) {
 	ShaperCalibration.awaken();
 	GpioKeys.awaken();
 	TempGraph.awaken();
+	Network.awaken();
 	console.log("X1Plus.js is awake");
 }
 
