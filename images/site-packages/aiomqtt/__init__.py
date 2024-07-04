4bb0dc4c9d001982443c36aa3e92b00bb4c203e2
Joshua Wise <joshua@joshuawise.com>
2024-07-04 02:44:24 -0400

    Vendor in aiomqtt-2.2.0 and paho_mqtt-2.1.0.

---
+++ images/site-packages/aiomqtt/__init__.py
@@ -0,0 +1,33 @@
+# SPDX-License-Identifier: BSD-3-Clause
+from .client import (
+    Client,
+    ProtocolVersion,
+    ProxySettings,
+    TLSParameters,
+    Will,
+)
+from .exceptions import MqttCodeError, MqttError, MqttReentrantError
+from .message import Message
+from .topic import Topic, TopicLike, Wildcard, WildcardLike
+
+# These are placeholders that are managed by poetry-dynamic-versioning
+__version__ = "2.2.0"
+__version_tuple__ = (2, 2, 0)
+
+__all__ = [
+    "__version__",
+    "__version_tuple__",
+    "Client",
+    "Message",
+    "ProtocolVersion",
+    "ProxySettings",
+    "TLSParameters",
+    "Topic",
+    "TopicLike",
+    "Wildcard",
+    "WildcardLike",
+    "Will",
+    "MqttCodeError",
+    "MqttReentrantError",
+    "MqttError",
+]
