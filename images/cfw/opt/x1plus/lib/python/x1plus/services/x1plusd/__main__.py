faa44def4cdd8ab419e45029afedeb1f26e8a7ff
Joshua Wise <joshua@joshuawise.com>
2024-11-10 03:05:23 -0500

    Revert "Add check if x1plusd is already running (#373)"
    This reverts commit fe63ba1aedbe5e4091117bced9d9d4952f0dd172.

--- images/cfw/opt/x1plus/lib/python/x1plus/services/x1plusd/__main__.py
+++ images/cfw/opt/x1plus/lib/python/x1plus/services/x1plusd/__main__.py
@@ -1,6 +1,5 @@
 import asyncio
 import logging, logging.handlers
-import os
 import setproctitle
 
 from . import X1PlusDaemon, logger
@@ -19,8 +18,6 @@ ch.setLevel(logging.DEBUG)
 ch.setFormatter(logging.Formatter("[%(asctime)s] %(name)s: %(levelname)s: %(message)s"))
 logger.addHandler(ch)
 
-PID_FILE = '/var/run/x1plusd.pid'
-
 
 def exceptions(loop, ctx):
     logger.error(f"exception in coroutine: {ctx['message']} {ctx.get('exception', '')}")
@@ -35,36 +32,13 @@ async def start():
     x1plusd = await X1PlusDaemon.create()
     await x1plusd.start()
 
-
-def is_already_running():
-    try:
-        with open(PID_FILE, 'r') as f:
-            pid = int(f.read().strip())
-        os.kill(pid, 0)  # Check if the process is still running
-        return True
-    except (IOError, ValueError, OSError):
-        return False
-
-
-# catchall exception
+# TODO: check if we are already running
+loop = asyncio.new_event_loop()
+loop.set_exception_handler(exceptions)
+loop.create_task(start())
 try:
-    # Check if we are already running
-    if is_already_running():
-        logger.error("x1plusd is already running. Exiting.")
-        exit(1)
-
-    # Create the PID file
-    with open(PID_FILE, 'w') as f:
-        f.write(str(os.getpid()))
-
-    loop = asyncio.new_event_loop()
-    loop.set_exception_handler(exceptions)
-    loop.create_task(start())
     loop.run_forever()
-except Exception as e:
-    logger.error(f"Exception occurred: {e}")
 finally:
     logger.error("x1plusd event loop has terminated!")
     loop.run_until_complete(loop.shutdown_asyncgens())
-    os.unlink(PID_FILE)
     loop.close()
